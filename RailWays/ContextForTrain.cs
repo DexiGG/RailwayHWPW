0769c8ac05da9aa200bc93fa97e28912de675d8b
DexiGG <invokedex@gmail.com>
2019-05-02 02:39:36 +0600

    RailwayHWPW

---
+++ RailWays/ContextForTrain.cs
@@ -0,0 +1,34 @@
+﻿namespace RailWays
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ContextForTrain : DbContext
+    {
+        // Контекст настроен для использования строки подключения "ContextForTrain" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "RailWays.ContextForTrain" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "ContextForTrain" 
+        // в файле конфигурации приложения.
+        public ContextForTrain()
+            : base("name=ContextForTrain")
+        {
+        }
+        public DbSet<Direction> Directions { get; set; }
+        public DbSet<Train> Trains { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Users> User { get; set; }
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
